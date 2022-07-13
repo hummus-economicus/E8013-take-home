bfba47dab396e34fa2fd2130beb2aed761862721
Linnenbrink <108485595+Linnenbrink@users.noreply.github.com>
2022-07-13 15:33:38 +0200

    Update simulation.m

--- simulation.m
+++ simulation.m
@@ -3,17 +3,18 @@
 n_f=5000;
 n_w=50000;
 
-%
+% firms
 f_mat=zeros(3,n_f);
-f_mat[1,:]=1:n_f;
-f_mat[2,:]=sort(rand(1,n_f));
+f_mat(1,:)=[1:n_f];
+f_mat(2,:)=sort(randi([0 grid_size],1,n_f)/grid_size);
+kron(f_mat, ones(1,10))
 
 
 % start from stationary distribution
 % workers: record worker id, x, y (if matched), sigma (if matched), firm id (if matched)
 w_mat=zeros(5,n_w);
-w_mat[1,:]=1:n_w;
-w_mat[2,:]=sort(rand(1,n_w));
+w_mat(1,:)=[1:n_w];
+w_mat(2,:)=sort(randi([0 grid_size],1,n_w)/grid_size);
 
 
 % simulate 6 years
