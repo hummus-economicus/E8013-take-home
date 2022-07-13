5b750ceb52204c72885c839f8a52b81561f06d23
hummus-economicus <91894351+hummus-economicus@users.noreply.github.com>
2022-07-13 10:59:14 +0200

    Create E8013main.m

---
+++ E8013main.m
@@ -0,0 +1,27 @@
+
+clear all; 
+clc; 
+
+%% Calibration
+b = 0; alpha = 0.5; beta = 0.994; sigL = 0.02; sigH = 0.08;
+phi = 0.5; lambda = 0.3; grid_size = 500; 
+tol = 10^(-3);
+
+%% Inner Loop: VFI on Surplus 
+
+S_init = zeros(grid_size,grid_size,2);
+
+% trial with uniform distribution for unemployed and vacancies: 
+u = ones(1,grid_size)/grid_size;
+v = ones(1,grid_size,2)/grid_size;
+S_n = VFI_surplus(S_init,b,alpha,beta,sigL,sigH,phi,lambda,grid_size,u,v,tol);
+
+
+
+%% Plotting equilibrium matching sets conditional on job security sigma 
+
+
+
+
+
+
