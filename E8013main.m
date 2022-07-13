382c75a7f6780995b2f536924cc0167d357c8c00
hummus-economicus <91894351+hummus-economicus@users.noreply.github.com>
2022-07-13 15:16:17 +0200

    Update E8013main.m

--- E8013main.m
+++ E8013main.m
@@ -6,22 +6,58 @@ clc;
 b = 0; alpha = 0.5; beta = 0.994; sigL = 0.02; sigH = 0.08;
 phi = 0.5; lambda = 0.3; grid_size = 500; 
 tol = 10^(-3);
+MaxIt = 10^4; 
 
 
 %% Question 4: solve 
 
-% Inner Loop: VFI on Surplus 
 grid = linspace(0,1,grid_size);
 
+% Initial guess: 
 S_init = zeros(grid_size,grid_size,2); 
+u_init = ones(1,grid_size)/grid_size;
+v_init = ones(1,grid_size,2)/grid_size;
+v_init(:,:,1) = v_init(:,:,1)*phi ;
+v_init(:,:,1) = v_init(:,:,2)*(1-phi) ;
 
-% trial with uniform distribution for unemployed and vacancies: 
-u = ones(1,grid_size)/grid_size;
-v = ones(1,grid_size,2)/grid_size;
-v(:,:,1) = v(:,:,1)*phi 
-v(:,:,1) = v(:,:,2)*(1-phi) 
+% Inner Loop: VFI on Surplus 
+u_n = u_init; 
+v_n = v_init;
 
-S_n = VFI_surplus(S_init,b,alpha,beta,sigL,sigH,phi,lambda,grid_size,u,v,tol);
+v_n1 = nan(grid_size,1,2);
+u_n1 = nan(grid_size,1);
+
+check=1;
+it = 0
+while (check > tol & it < MaxIt)
+    it = it + 1; 
+    S_n1 = VFI_surplus(S_init,b,alpha,beta,sigL,sigH,phi,lambda,grid_size,u_n,v_n,tol,MaxIt);
+
+% update distributions 
+    
+    for y = 1:grid_size 
+       auxL = S_n1(:,y,1)>=0./( S_n1(:,y,1)<0 +sigL*(S_n1(:,y,1)>=0) ) 
+       auxH = S_n1(:,y,2)>=0./( S_n1(:,y,2)<0 +sigH*(S_n1(:,y,2)>=0) ) 
+
+       v_n1(:,:,1) = phi/(1 + u_n*lambda*auxL )
+       v_n1(:,:,2) = (1-phi)/(1 + u_n*lambda*auxH )
+    end 
+    for x = 1:grid_size
+        auxL = S_n1(x,:,1)>=0./( S_n1(x,:,1)<0 +sigL*(S_n1(x,:,1)>=0) ) 
+        auxH = S_n1(x,:,2)>=0./( S_n1(x,:,2)<0 +sigH*(S_n1(x,:,2)>=0) ) 
+        u_n1(x,:) = 1/(1 + phi*v_n(:,:,1)*lambda*auxL' + (1-phi)*v_n(:,:,2)*lambda*auxH' )
+    end
+    
+    check_v = max(  abs( v_n1-v_n ) , [], 1 );
+    check_u = max(  abs( u_n1-u_n ) , [], 1 );
+
+    check = max([check_v,check_u]);
+
+    S_init = S_n1; 
+    u_n = u_n1;
+    v_n = v_n1;
+    
+end 
 
 
 %% Question 5: Plotting equilibrium matching sets conditional on job security sigma 
@@ -52,10 +88,6 @@ plot(grid,up_h)
 patch([grid fliplr(grid)], [low_h' fliplr(up_h')], 'b')
 title("Matching set for low job security")
 hold off
-
-
-
-
 
 
 
