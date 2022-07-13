65817136b0e3061a7ee809fe30a6cf98816b271e
Linnenbrink <108485595+Linnenbrink@users.noreply.github.com>
2022-07-13 15:06:23 +0200

    Update simulation.m

--- simulation.m
+++ simulation.m
@@ -1,3 +1,16 @@
+% Question 8
+
 n_f=5000;
 n_w=50000;
 
+%
+f_mat=zeros(3,n_w);
+
+
+% start from stationary distribution
+% workers: record worker id, x, y (if matched), sigma (if matched), firm id (if matched)
+w_mat=zeros(5,n_w);
+
+
+% simulate 6 years
+T=72;
