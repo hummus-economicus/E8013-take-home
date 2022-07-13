977f91459a63ed7146e3621e7c4bd5d2ab21b34b
hummus-economicus <91894351+hummus-economicus@users.noreply.github.com>
2022-07-13 10:59:39 +0200

    Create VFI_surplus.m

---
+++ VFI_surplus.m
@@ -0,0 +1,57 @@
+function S_n = VFI_surplus(S_init,b,alpha,beta,sigL,sigH,phi,lambda,grid_size,u,v,tol)
+% -------------------------------------------------------------------------
+% This function performs a value function ...
+% 
+% Inputs:
+% - S_init: initial guess for surplus function 
+% - ... other parameters from exercise 
+% - u: density of unemployed (1xgrid_size) vector
+% - v: density of vacancies  (1xgrid_sizex2) vector
+% - tol: tolerance value for convergence 
+% -------------------------------------------------------------------------
+
+% Creating equidistant grid for productivities x,y
+grid = linspace(0,1,grid_size);
+
+flow = nan(grid_size, grid_size);
+S_plus = nan(grid_size, grid_size,2);
+S_n1 = nan(grid_size, grid_size, 2);
+
+% x in rows
+% y in columns 
+flow = repmat(grid,grid_size,1).*repmat(grid',1,grid_size)-b;
+
+S_n = S_init;
+
+check_S=1;
+while (check_S > tol)
+
+    % define S_plus:
+    S_plus = max(S_n,0);
+    U = sum(u);
+    V = phi*sum(v(:,:,1),'all') + (1-phi)*sum(v(:,:,2),'all');
+    
+for x = 1:grid_size
+    for y = 1:grid_size 
+        S_n1(x,y,1) = flow(x,y) - b ...
+                        - (1-alpha)*beta*lambda*S_plus(:,y,1)'*u'/U ...
+                        - alpha*beta*lambda*( phi*S_plus(x,:,1)*v(:,:,1)'/V ...
+                        + (1-phi)*S_plus(x,:,2)*v(:,:,1)'/V) ;
+        S_n1(x,y,2) = flow(x,y) - b ...
+                        - (1-alpha)*beta*lambda*S_plus(:,y,1)'*u'/U ...
+                        - alpha*beta*lambda*( phi*S_plus(x,:,1)*v(:,:,1)'/V ...
+                        + (1-phi)*S_plus(x,:,2)*v(:,:,1)'/V) ;
+        S_n1(x,y,1) = (1-(1-sigL)*beta)^(-1)*S_n1(x,y,1) ;
+        S_n1(x,y,2) = (1-(1-sigH)*beta)^(-1)*S_n1(x,y,2) ;
+    end 
+end
+    
+    check_S_h = max(  abs( S_n1(:,:,1)-S_n(:,:,1) ) , [], 1 );
+    check_S_l = max(  abs( S_n1(:,:,2)-S_n(:,:,2) ) , [], 1 );
+    check_S = max(check_S_h,check_S_l);
+    
+    S_n = S_n1;
+    
+end
+
+end
