ee1b76ac3fb6e64b057315a1d886afb0e9b7858a
hummus-economicus <91894351+hummus-economicus@users.noreply.github.com>
2022-07-14 17:40:58 +0200

    Add files via upload

---
+++ regression.R
@@ -0,0 +1,12 @@
+cat("\014")      # clean console
+library(dplyr)
+library(readxl)
+library(fixest)
+table <- read_excel("Downloads/E8013-take-home/regression_table.xlsx")
+table <- rename(table, worker_ID = W_1)
+table <- rename(table, firm_ID = W_2)
+table <- rename(table, loq_wage_ID = W_3)
+
+
+reg = feols(table$loq_wage_ID ~ 1 |worker_ID + firm_ID , data = table)
+
