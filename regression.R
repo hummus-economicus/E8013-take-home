a61a0e3268acb117582e24a53a693e55d81de736
Ulrich Roschitsch <62427891+uroschit@users.noreply.github.com>
2022-07-14 17:51:21 +0200

    regression

--- regression.R
+++ regression.R
@@ -2,7 +2,7 @@ cat("\014")      # clean console
 library(dplyr)
 library(readxl)
 library(fixest)
-table <- read_excel("Downloads/E8013-take-home/regression_table.xlsx")
+table <- read_excel("regression_table.xlsx")
 table <- rename(table, worker_ID = W_1)
 table <- rename(table, firm_ID = W_2)
 table <- rename(table, loq_wage_ID = W_3)
