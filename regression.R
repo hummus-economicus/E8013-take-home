07a3c178d7126d539b68f2ea6ff43f8181d8842f
Ulrich Roschitsch <62427891+uroschit@users.noreply.github.com>
2022-07-14 18:38:13 +0200

    regression

--- regression.R
+++ regression.R
@@ -5,8 +5,32 @@ library(fixest)
 table <- read_excel("regression_table.xlsx")
 table <- rename(table, worker_ID = W_1)
 table <- rename(table, firm_ID = W_2)
-table <- rename(table, loq_wage_ID = W_3)
+table <- rename(table, loq_wage = W_3)
 
 
-reg = feols(table$loq_wage_ID ~ 1 |worker_ID + firm_ID , data = table)
+reg = feols(table$loq_wage ~ 1 | worker_ID + firm_ID , data = table)
 
+fixeffs <- fixef(reg,sorted = TRUE)
+table[,c("worker FE", "firm FE")] <- NA
+for (n in 1:length(fixeffs[["firm_ID"]])) {
+  table[["firm FE"]][table$firm_ID == n] <- fixeffs[["firm_ID"]][n]
+}
+for (n in 1:length(fixeffs[["worker_ID"]])) {
+  table[["worker FE"]][table$worker_ID == n] <- fixeffs[["worker_ID"]][n]
+}
+
+# headline
+cor(table$`worker productivity`, table$`firm productivity`, method = "pearson", use = "complete.obs")
+cor(table$`worker FE`, table$`firm FE`, method = "pearson", use = "complete.obs")
+cor(table$`worker FE`, table$`worker productivity`, method = "pearson", use = "complete.obs")
+cor(table$`firm FE`, table$`firm productivity`, method = "pearson", use = "complete.obs")
+# stable matches
+cor(table[table$stable]$`worker productivity`, table[table$stable]$`firm productivity`, method = "pearson", use = "complete.obs")
+cor(table[table$stable]$`worker FE`, table[table$stable]$`firm FE`, method = "pearson", use = "complete.obs")
+cor(table[table$stable]$`worker FE`, table[table$stable]$`worker productivity`, method = "pearson", use = "complete.obs")
+cor(table[table$stable]$`firm FE`, table[table$stable]$`firm productivity`, method = "pearson", use = "complete.obs")
+# unstable matches
+cor(table[!table$stable]$`worker productivity`, table[!table$stable]$`firm productivity`, method = "pearson", use = "complete.obs")
+cor(table[!table$stable]$`worker FE`, table[!table$stable]$`firm FE`, method = "pearson", use = "complete.obs")
+cor(table[!table$stable]$`worker FE`, table[!table$stable]$`worker productivity`, method = "pearson", use = "complete.obs")
+cor(table[!table$stable]$`firm FE`, table[!table$stable]$`firm productivity`, method = "pearson", use = "complete.obs")
